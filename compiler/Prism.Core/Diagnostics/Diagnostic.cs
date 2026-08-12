679ed10379229ef2f9b315799244742c11607874
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2026-08-11 20:35:25 -0400

    Created the beginings of the C# frontend port

---
+++ compiler/Prism.Core/Diagnostics/Diagnostic.cs
@@ -0,0 +1,32 @@
+﻿// @file Diagnostic.cs
+//
+// @copyright Copyright (c) 2026 Retro & Chill. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+namespace Prism.Core.Diagnostics;
+
+public sealed class Diagnostic
+{
+    internal Diagnostic(DiagnosticInfo info, Location location, bool isSuppressed = false)
+    {
+        Info = info;
+        Location = location;
+        IsSuppressed = isSuppressed;
+    }
+
+    internal DiagnosticInfo Info { get; }
+
+    public Location Location { get; }
+
+    public bool IsSuppressed { get; }
+
+    public DiagnosticDescriptor Descriptor => Info.Descriptor;
+
+    public string Id => Descriptor.Id;
+
+    public string Category => Descriptor.Category;
+
+    public DiagnosticSeverity Severity => Info.Severity;
+
+    public string GetMessage() => Info.GetMessage();
+}
---
+++ compiler/Prism.Core/Diagnostics/SyntaxDiagnosticInfo.cs
@@ -0,0 +1,15 @@
+﻿// @file SyntaxDiagnosticInfo.cs
+//
+// @copyright Copyright (c) 2026 Retro & Chill. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using Prism.Core.Text;
+
+namespace Prism.Core.Diagnostics;
+
+internal readonly record struct SyntaxDiagnosticInfo(TextSpan RelativeSpan, DiagnosticInfo Info)
+{
+    public int Offset => RelativeSpan.Start;
+
+    public int Width => RelativeSpan.Length;
+}
